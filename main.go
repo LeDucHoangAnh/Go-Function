3d4414e7c36668a7637d4a5763cac763a1a771a6
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 00:32:00 +0700

    func kèm return type.

--- main.go
+++ main.go
@@ -11,7 +11,13 @@ func HelloWithParam(name string) {
 	fmt.Println("Hello", name)
 }
 
+func greeting(name string) string {
+	result := fmt.Sprintf("Hello %s", name)
+	return result
+}
 func main() {
 	Hello()
 	HelloWithParam("World")
+	result := greeting("Kai")
+	fmt.Println(result)
 }
