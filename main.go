7271bfbf994251fafcc5d896e64a74554aab07de
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 00:34:53 +0700

    Multiple return value .

--- main.go
+++ main.go
@@ -15,9 +15,19 @@ func greeting(name string) string {
 	result := fmt.Sprintf("Hello %s", name)
 	return result
 }
+
+//Multiple return values
+func rectInfo(w int, h int) (int, int) {
+	return w, h
+}
+
 func main() {
 	Hello()
 	HelloWithParam("World")
 	result := greeting("Kai")
 	fmt.Println(result)
+
+	w, h := rectInfo(100, 200)
+	fmt.Println("Width = ", w)
+	fmt.Println("Height = ", h)
 }
